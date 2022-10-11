d63757b19992d7ed0020820a1d6da3ecaefd10b9
David <davidtheodore@Davids-MBP.home>
2022-10-11 11:47:51 -0500

    adding some vulnerable funcs

--- target.go
+++ target.go
@@ -1,37 +1,45 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"strings"
 )
 
 type StringToAdd struct {
-        Name string
+	Name string
+}
+
+type ComplexStruct struct {
+	Name              string
+	HexRepresentation []byte
 }
 
 func main() {
-    hello := "Hello"
-    world := "World"
-    words := []string{hello, world}
-    SayHello(words)
-    s := StringToAdd{Name:"add to me "}
-    fmt.Println(s.AppendFoo())
-    Int := IntToAdd{A:5}
-    fmt.Println(Int.AppendInt(10))
+	hello := "Hello"
+	world := "World"
+	words := []string{hello, world}
+	SayHello(words)
+	s := StringToAdd{Name: "add to me "}
+	fmt.Println(s.AppendFoo())
+	Int := IntToAdd{A: 18446744073709551615}
+	fmt.Println(Int.AppendInt(10))
+	//c := ComplexStruct{}
+	//c.SetNameViaHex("48656c6c6f20476f7068657221")
 }
 
 // SayHello says Hello
 func SayHello(words []string) {
-    fmt.Println(JoinStrings(words))
+	fmt.Println(JoinStrings(words))
 }
 
 // JoinStrings joins strings
 func JoinStrings(words []string) string {
-    return strings.Join(words, ", ")
+	return strings.Join(words, ", ")
 }
 
-func AddInts(int1 IntToAdd, int2 int) int {
-        return int1.A + int2
+func AddInts(int1 IntToAdd, int2 uint64) uint64 {
+	return int1.A + int2
 }
 
 func (s StringToAdd) AppendFoo() string {
@@ -39,9 +47,20 @@ func (s StringToAdd) AppendFoo() string {
 }
 
 type IntToAdd struct {
-        A int
+	A uint64
 }
 
-func (a IntToAdd) AppendInt(b int) int {
+func (a IntToAdd) AppendInt(b uint64) uint64 {
 	return a.A + b
+}
+
+func (c ComplexStruct) SetNameViaHex(src []byte) {
+
+	dst := make([]byte, hex.DecodedLen(len(src)))
+	n, err := hex.Decode(dst, src)
+	if err != nil {
+		panic(err)
+	}
+	c.Name = string(dst[:n])
+	fmt.Printf("ComplexStruct name set to %s", c.Name)
 }
